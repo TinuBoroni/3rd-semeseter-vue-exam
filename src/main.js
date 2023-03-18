4c64fe7171ec7fe5edf6bd3278a1a4b8891c4f5a
Arietecodes <phiestytinuboroni@gmail.com>
2023-03-18 23:34:52 +0530

    new changes

---
+++ src/main.js
@@ -0,0 +1,26 @@
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import "./styles.css";
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD3AoOGfAMdV4xMK9A4dsC-efRXS4gzSFI",
+  authDomain: "codeschool-67536.firebaseapp.com",
+  projectId: "codeschool-67536",
+  storageBucket: "codeschool-67536.appspot.com",
+  messagingSenderId: "431379352806",
+  appId: "1:431379352806:web:851af27bc95162d2f63f27",
+  measurementId: "G-EXK8W3W65R"
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
+
+createApp(App).use(store).use(router).mount("#app");
---
+++ src/views/HomeView.vue
@@ -0,0 +1,31 @@
+<template>
+  <section class="bg-white dark:bg-gray-900">
+    <div class="container px-6 py-16 mx-auto text-center">
+      <div class="max-w-lg mx-auto">
+        <h1
+          class="text-3xl font-semibold text-gray-800 dark:text-white lg:text-4xl"
+        >
+          Building Your Confidence and Trust with our Awesome Products
+        </h1>
+        <p class="mt-6 text-gray-500 dark:text-gray-300">
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Libero
+          similique obcaecati illum mollitia.
+        </p>
+      </div>
+
+      <div class="flex justify-center mt-10">
+        <img
+          class="object-cover w-full h-96 rounded-xl lg:w-4/5"
+          :src="requiure('public/Bicycle workshop-cuate.svg')"
+        />
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+export default defineComponent({
+  name: "HomeView",
+});
+</script>
